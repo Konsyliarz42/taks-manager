83983325204ff3de9de7d64b8fc0c56f611989be
Tomasz Kordiak <dotomika@gmail.com>
2022-07-30 12:43:33 +0200

    Remove migrations

--- task_manager/__main__.py
+++ task_manager/__main__.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser, MetavarTypeHelpFormatter
-from datetime import date
 
-from . import api, app, router
+from . import api, app
 from .resources import TaskList, UserList
 
 
@@ -16,49 +15,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(prog="task_manager")
     parser.set_defaults(cmd=parser.prog)
     subparsers = parser.add_subparsers()
-
-    # --------------------------------
-
-    migrate_parser = subparsers.add_parser(
-        "migrate",
-        formatter_class=MetavarTypeHelpFormatter,
-        description="Run all migrations or apply to selected migration.",
-    )
-    migrate_parser.set_defaults(cmd=migrate_parser.prog)
-
-    migrate_parser.add_argument(
-        "--selected-migration",
-        help="Name of migration to apply",
-        type=str,
-        required=False,
-    )
-
-    # --------------------------------
-
-    makemigrations_parser = subparsers.add_parser(
-        "makemigrations",
-        formatter_class=MetavarTypeHelpFormatter,
-        description="Generate a new migration in your migrations folder or prepare template for your own migration.",
-    )
-    makemigrations_parser.set_defaults(cmd=makemigrations_parser.prog)
-
-    makemigrations_parser.add_argument(
-        "--migration-name",
-        help="Name of migration | Default: migration_<date>",
-        default=f"migrate_{date.today()}",
-        type=str,
-        required=False,
-    )
-    makemigrations_parser.add_argument(
-        "--auto",
-        help="Auto generated migration | Default: True",
-        default=True,
-        type=bool,
-        required=False,
-        choices=[True, False],
-    )
-
-    # --------------------------------
 
     runserver_parser = subparsers.add_parser(
         "runserver",
@@ -93,18 +49,5 @@ if __name__ == "__main__":
     # --------------------------------
 
     args = parser.parse_args()
+    main(host=args.host, port=args.port, debug=args.debug)
 
-    if args.cmd == runserver_parser.prog:
-        main(host=args.host, port=args.port, debug=args.debug)
-    elif args.cmd == migrate_parser.prog:
-        if args.selected_migration:
-            router.run(name=args.selected_migration)
-        else:
-            router.run()
-    elif args.cmd == makemigrations_parser.prog:
-        router.create(
-            name=args.migration_name,
-            auto=args.auto,
-        )
-    else:
-        parser.print_help()
